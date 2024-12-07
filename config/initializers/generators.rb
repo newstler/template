07291ed6ed6917727c3f7b20079d9faa82289472
Yuri Sidorov <403994+newstler@users.noreply.github.com>
2024-12-08 00:17:49 +0100

    WIP: Separate API logic.

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,3 +1,16 @@
 Rails.application.config.generators do |g|
   g.orm :active_record, primary_key_type: :string
+
+  g.after_generate do |files|
+    next if files.grep(/controller\.rb/).empty?
+
+    controller_name = files.grep(/controller\.rb/).first.split('/').last.gsub('_controller.rb', '')
+
+    Rails::Generators.invoke "scaffold_controller", [
+      "api/v1/#{controller_name}",
+      *ARGV.drop(1),
+      "--api",
+      "--force"
+    ]
+  end
 end
